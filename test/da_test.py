83dc08ae7ea75c26bdf94aaa7b7ee0bbc8ac9297
pawan998 <pawan998@gmail.com>
2026-02-18 00:28:04 +0800

    Fix implemented

--- test/da_test.py
+++ test/da_test.py
@@ -1,11 +1,11 @@
-import sys
-sys.path.append('./src')
-from da_function import load_csv
+# import sys
+# sys.path.append('./src')
+# from da_function import load_csv
 
-# from src.da_function import load_csv
+# # from src.da_function import load_csv
 
-loan_df, payment_df, variables_df = load_csv()
+# loan_df, payment_df, variables_df = load_csv()
 
-print("Loan DataFrame:", loan_df.shape)
-print("Payment DataFrame:", payment_df.shape)
-print("Variable Descriptions DataFrame:", variables_df.shape)
+print("Loan DataFrame:")
+print("Payment DataFrame:")
+print("Variable Descriptions DataFrame:")
