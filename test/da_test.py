6b2e4946c9057f19da45c9cf7fef3adadfd26570
pawan998 <pawan998@gmail.com>
2026-02-18 13:12:45 +0800

    fix attempt 02

--- test/da_test.py
+++ test/da_test.py
@@ -2,11 +2,14 @@ import sys
 sys.path.append('./src')
 from da_function import addition
 
-src_val = addition(2, 3)
+a = 2
+b = 3
+
+src_val = addition(a, b)
 
 def test_addition(a, b, src_val):
     test_value = a + b
     print("source value:", src_val, ", Test Value:", test_value, ": Soruce value and test values are equal.")
     print("Testing successful!")
 
-test_addition(2, 3, src_val)
+test_addition(a, b, src_val)
